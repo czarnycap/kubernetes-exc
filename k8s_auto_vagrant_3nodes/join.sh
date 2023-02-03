44272d43d83f352c52628773528f860e495f2945
akowal <akowal@sslaptop.local>
2023-02-03 15:45:15 +0100

    working setup with automatically assigned bridge network
    	modified:   .gitignore

--- k8s_auto_vagrant_3nodes/join.sh
+++ k8s_auto_vagrant_3nodes/join.sh
@@ -1,3 +1,2 @@
-kubeadm join 192.168.56.11:6443 --token 3qq3jl.vkj2tpa90sjqqpwb \
---cri-socket=unix:///var/run/cri-dockerd.sock 
---discovery-token-unsafe-skip-ca-verification 
+kubeadm join 192.168.56.11:6443 --token 8cux2p.7fzmdw9w67qy1f1g \
+--cri-socket=unix:///var/run/cri-dockerd.sock --discovery-token-unsafe-skip-ca-verification
