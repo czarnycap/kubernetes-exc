d205d8693ed0bcca0fa539e3899155e922923ddb
akowal <akowal@sslaptop.local>
2023-02-03 21:19:21 +0100

    done

--- k8s_auto_vagrant_3nodes/join.sh
+++ k8s_auto_vagrant_3nodes/join.sh
@@ -1,2 +1,2 @@
-kubeadm join 192.168.56.11:6443 --token 8cux2p.7fzmdw9w67qy1f1g \
+kubeadm join 192.168.56.11:6443 --token g7lp89.uwe7uqeb09r8z9cw \
 --cri-socket=unix:///var/run/cri-dockerd.sock --discovery-token-unsafe-skip-ca-verification
