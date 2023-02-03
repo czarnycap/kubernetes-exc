494ce2e6a6bbb9f582fc7c2079c76ff5a7ffdf49
akowal <akowal@sslaptop.local>
2023-02-03 21:31:37 +0100

    changed iface to enp0s8 (experiment)

--- k8s_auto_vagrant_3nodes/join.sh
+++ k8s_auto_vagrant_3nodes/join.sh
@@ -1,2 +1,2 @@
-kubeadm join 192.168.56.11:6443 --token g7lp89.uwe7uqeb09r8z9cw \
+kubeadm join 192.168.56.11:6443 --token b1c8sl.dkmo2tslqx4wkjgl \
 --cri-socket=unix:///var/run/cri-dockerd.sock --discovery-token-unsafe-skip-ca-verification
