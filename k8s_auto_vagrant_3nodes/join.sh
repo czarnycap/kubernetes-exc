a1a575183ae951e07ad5224b35f5b5226e1a2e7a
akowal <akowal@sslaptop.local>
2023-02-09 13:57:21 +0100

    refactor only

--- k8s_auto_vagrant_3nodes/join.sh
+++ k8s_auto_vagrant_3nodes/join.sh
@@ -1,2 +1,2 @@
-kubeadm join 192.168.56.11:6443 --token b1c8sl.dkmo2tslqx4wkjgl \
+kubeadm join 192.168.56.11:6443 --token 44ln2l.tvuvp0hcfh9kwn7r \
 --cri-socket=unix:///var/run/cri-dockerd.sock --discovery-token-unsafe-skip-ca-verification
